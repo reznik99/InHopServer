85a730135dcb98be2ee2c649e9a3d92a31aa65eb
francesco gorini <francesco.gorini2000@yahoo.com>
2020-10-08 22:08:09 +1300

    Fixed loading env file.

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
 
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
-  ssl: false,
+    connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+    ssl: false,
 });
 
 module.exports = { pool };
